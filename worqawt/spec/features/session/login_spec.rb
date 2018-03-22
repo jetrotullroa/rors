b5e305c7cc1368bf69876dee7791fb8c32b96315
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-03-22 19:59:02 +0800

    add spec for logging in a user

---
+++ worqawt/spec/features/session/login_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "User Login" do
+  before do
+    @john = User.create!(email: "john@example.com", password: "password")
+  end
+
+  scenario do
+    visit "/"
+
+    click_link "Login"
+    fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Login"
+
+    expect(page).to have_link("Logout")
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
