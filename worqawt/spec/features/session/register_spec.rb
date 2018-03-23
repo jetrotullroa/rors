0af790629174491e0f0e1ded690c542ce8207187
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-03-23 09:48:01 +0800

    restyle session form using simple form for

--- worqawt/spec/features/session/register_spec.rb
+++ worqawt/spec/features/session/register_spec.rb
@@ -5,9 +5,9 @@ RSpec.feature "Register a new User" do
     visit "/"
 
     click_link "Register"
-    fill_in "Email", with: "johndoe@example.com"
-    fill_in "Password", with: "passwords"
-    fill_in "Confirm Password", with: "passwords"
+    fill_in "Email *", with: "johndoe@example.com"
+    fill_in "Password *", with: "passwords"
+    fill_in "Confirm Password *", with: "passwords"
     click_button "Register"
 
     expect(page).to have_link("Logout")
