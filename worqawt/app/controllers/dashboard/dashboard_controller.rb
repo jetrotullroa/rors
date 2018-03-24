9d7bc63898950aac0a887cfbcab2baed41aae556
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-03-24 13:08:48 +0800

    create a dashboard index that will show all user exercise

--- worqawt/app/controllers/dashboard/dashboard_controller.rb
+++ worqawt/app/controllers/dashboard/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Dashboard::DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
-    @exercises = Exercise.all.where(user_id: current_user)
+    @exercises = Exercise.all.where(user_id: current_user).order(created_at: :desc)
   end
 end
