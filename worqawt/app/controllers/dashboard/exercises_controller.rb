72a86979d469ed349b079b25b96d2ae1259d5108
Jetro Tull Roa <jetrotullroa@gmail.com>
2018-03-24 13:09:39 +0800

    setup page for creating new exercise/workout

--- worqawt/app/controllers/dashboard/exercises_controller.rb
+++ worqawt/app/controllers/dashboard/exercises_controller.rb
@@ -1,7 +1,22 @@
 class Dashboard::ExercisesController < ApplicationController
   def new
+    @exercise = Exercise.new
   end
 
   def create
+    @exercise = current_user.exercises.new(exercise_params)
+    if @exercise.save
+      redirect_to dashboard_root_path
+      flash[:notice] = "Exercise has been completed."
+    else
+      render :new
+    end
   end
+
+
+  private
+
+    def exercise_params
+      params.require(:exercise).permit(:duration, :workout)
+    end
 end
